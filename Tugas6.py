885a18b9c29c600aa5d54887ce73c907a2592eb2
gilangzrutz <gilangxd942@gmail.com>
2024-11-08 09:30:01 +0800

    Add files via upload

---
+++ Tugas6.py
@@ -0,0 +1,25 @@
+# variabel assigment value
+x = 9
+y = 6
+print("hasil dari x adalah: ", y)
+
+# aturan penamaan variabel
+luas = 30 # bisa berupa kata
+edy_ganteng = 100 # jika dua kata, digabung dg " "
+KeziaX = 10 # boleh gabungan kapital non kapital
+keziaX = 3
+lencana88 = 10 # jika menggabungkan huruf dan angka
+print("nilai dari kezia adalah : ", lencana88)
+
+
+a = 4 # integral bilangan bulat
+print("tipe data dari a : ", type (a))
+
+b = 6.9 # float bilangan pecahan
+print("tipe data dari b : ", type (b))
+
+c = "gacor" # string gabungan karakter
+print("tipe data dari a : ", type (c))
+
+d = False # Boolen, true false 
+print("tipe data dari a : ", type (d))
