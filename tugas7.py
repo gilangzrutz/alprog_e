462fe337e407d6c7dd5f065ce0fca9df1bf634c0
gilangzrutz <gilangxd942@gmail.com>
2024-11-19 15:52:33 +0800

    Add files via upload

---
+++ tugas7.py
@@ -0,0 +1,6 @@
+print(" >>>>> menghitung luas persegi <<<<< ")
+panjang = int(input("masukkan panjang: "))
+lebar = int(input("masukkan lebar: "))
+      
+hasil = panjang * lebar
+print(f"hasil dari {panjang} x {lebar} = { hasil}")
