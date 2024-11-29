cad282637f580799b4d4de2e961a454d3bb7aa6e
gilangzrutz <gilangxd942@gmail.com>
2024-11-29 16:39:33 +0800

    Add files via upload

---
+++ Tugas 8.py.py
@@ -0,0 +1,32 @@
+#aritmatika
+
+x = 24
+y = 66
+
+# Operasi Penjumlahan
+hasil = x + y
+print("nilai dari x + y = ", hasil)
+
+# Operasi Pengurangan
+hasil = x - y
+print("hasil dari x - y = ", hasil)
+
+# Operasi Pembagian 
+hasil = x / y
+print("hasil dari x / y = ", hasil)
+
+# Operasi Perkalian
+hasil = x * y
+print("hasil dari x * y =", hasil)
+
+# Operasi Exsponen
+hasil = x ** y
+print("hasil dari x ** y = ", hasil)
+
+# Operasi Modulus
+hasil = x % y
+print("hasil dari x % y = ", hasil)
+
+# Operasi Floor Division
+hasil = x // y
+print("hasil dari x // y = ", hasil)
