e738ad211b72b426fa3e7353a402caf7672fe8c6
gilangzrutz <gilangxd942@gmail.com>
2024-12-07 20:55:33 +0800

    Add files via upload

---
+++ Tugas9.py
@@ -0,0 +1,11 @@
+#operasi aritmatika
+nilai = float(input("masukkan nilai suhu dalam celcius :"))
+
+Fahrenheit = (nilai * 9/5)+32
+print("nilai dari Fahrenheit = ", Fahrenheit)
+
+Kelvin = nilai + 273,15
+print("nilai dari Kelvin = ", Kelvin)
+
+Reamur = nilai * (5/4)+0
+print("nilai dari Reamur = ", Reamur)
